f6c15776abdf83e8340f103710a45bebb4a973de
清茶 <2521541557@qq.com>
2025-03-06 15:44:53 +0800

    perf(page): 增加底部安全距离样式

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -9,7 +9,15 @@
 </route>
 
 <template>
-  <view class="min-h-screen" :style="{ marginTop: safeAreaInsets?.top + 'px' }">
+  <view
+    class="min-h-screen"
+    :style="{
+      marginTop: safeAreaInsets?.top + 'px',
+      marginBottom: safeAreaInsets?.bottom + 10 + 'px',
+    }"
+  >
+    <!-- 新增固定背景层 -->
+    <view class="fixed-bg"></view>
     <!-- 顶部标题 -->
     <view class="px-4 text-center">
       <text class="text-lg font-medium">我的</text>
@@ -20,6 +28,19 @@
 <script lang="ts" setup>
 const { safeAreaInsets } = uni.getSystemInfoSync()
 </script>
+
+<style>
+/* 新增固定背景层 */
+.fixed-bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(180deg, #c4dcfe 0%, #f5f5f5 45%);
+}
+</style>
 
 <style lang="scss" scoped>
 //
