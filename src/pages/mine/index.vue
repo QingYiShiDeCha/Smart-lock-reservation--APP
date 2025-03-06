dd7acd64098dbcb4e44765e1fa8de1d6203751ac
清茶 <2521541557@qq.com>
2025-03-06 14:02:12 +0800

    feat(page): 我的页面

---
+++ src/pages/mine/index.vue
@@ -0,0 +1,26 @@
+<route lang="json5" type="page">
+{
+  layout: 'default',
+  style: {
+    navigationStyle: 'custom',
+    navigationBarTitleText: '首页',
+  },
+}
+</route>
+
+<template>
+  <view class="min-h-screen" :style="{ marginTop: safeAreaInsets?.top + 'px' }">
+    <!-- 顶部标题 -->
+    <view class="px-4 text-center">
+      <text class="text-lg font-medium">我的</text>
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+const { safeAreaInsets } = uni.getSystemInfoSync()
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
