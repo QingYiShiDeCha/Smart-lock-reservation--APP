3b378f07113bdbe06162fc3ae45d021c0ce7b44f
清茶 <2521541557@qq.com>
2025-03-06 15:44:11 +0800

    perf(page): 增加底部安全距离样式

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -8,7 +8,15 @@
 }
 </route>
 <template>
-  <view class="min-h-screen" :style="{ marginTop: safeAreaInsets?.top + 'px' }">
+  <view
+    class="min-h-screen"
+    :style="{
+      marginTop: safeAreaInsets?.top + 'px',
+      marginBottom: safeAreaInsets?.bottom + 10 + 'px',
+    }"
+  >
+    <!-- 新增固定背景层 -->
+    <view class="fixed-bg"></view>
     <!-- 顶部标题 -->
     <view class="px-4 text-center">
       <text class="text-lg font-medium">首页</text>
@@ -158,16 +166,16 @@ const lockInfo = ref([
 </script>
 
 <style>
-page {
-  background: linear-gradient(180deg, #c4dcfe 0%, #f5f5f5 45%, #f5f5f5 100%);
+/* 新增固定背景层 */
+.fixed-bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(180deg, #c4dcfe 0%, #f5f5f5 45%);
 }
-/* #ifdef MP-WEIXIN */
-page {
-  height: 100%;
-  overflow: scroll;
-  background: linear-gradient(180deg, #c4dcfe 0%, #f5f5f5 45%, #f5f5f5 100%);
-}
-/* #endif */
 </style>
 
 <style lang="scss"></style>
