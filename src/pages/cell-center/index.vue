0eede67c30b25a2524894d2507b8dd3654407544
清茶 <2521541557@qq.com>
2025-03-07 11:02:53 +0800

    feat(page): 改用自定义组件

--- src/pages/cell-center/index.vue
+++ src/pages/cell-center/index.vue
@@ -27,67 +27,60 @@
       custom-style="background-color: #c6ddfe !important;"
       :bordered="false"
     />
-    <view class="fixed z-99 left-4 top-90rpx flex gap-30rpx text-36rpx">
-      <view @click="handleSwitchTag(1)">电话联系</view>
-      <view @click="handleSwitchTag(2)">微信联系</view>
-    </view>
-    <template v-if="activeTag === 1">
-      <view
-        :style="{
-          marginTop: safeHeight + 'px',
-        }"
-        class="flex flex-col box-border overflow-hidden"
-      >
-        <view class="flex flex-col box-border" v-for="(item, index) in phoneInfo" :key="index">
-          <view class="bg-white rounded-lg mt-30rpx flex flex-col p-4 gap-6rpx">
-            <view class="flex justify-between">
-              <view>{{ item.name }}</view>
-              <view class="text-24rpx px-3 rounded-lg bg-[#edfef6] text-center leading-normal">
-                值班中
+
+    <!-- 使用自定义标签页组件 -->
+    <custom-tab v-model="activeTab" sticky>
+      <custom-tab-item title="电话联系">
+        <view class="flex flex-col box-border overflow-hidden">
+          <view class="flex flex-col box-border" v-for="(item, index) in phoneInfo" :key="index">
+            <view class="bg-white rounded-lg mt-30rpx flex flex-col p-4 gap-6rpx">
+              <view class="flex justify-between">
+                <view>{{ item.name }}</view>
+                <view class="text-24rpx px-3 rounded-lg bg-[#edfef6] text-center leading-normal">
+                  值班中
+                </view>
               </view>
-            </view>
-            <view class="b-1 b-solid border-#f5f5f5"></view>
-            <view class="flex justify-between">
-              <view>{{ item.phone }}</view>
-              <view class="rounded-full bg-green p-8rpx">
-                <view class="i-carbon:phone-filled bg-white text-20rpx px-8rpx"></view>
+              <view class="b-1 b-solid border-#f5f5f5"></view>
+              <view class="flex justify-between">
+                <view>{{ item.phone }}</view>
+                <view class="rounded-full bg-green p-8rpx">
+                  <view class="i-carbon:phone-filled bg-white text-20rpx px-8rpx"></view>
+                </view>
               </view>
             </view>
           </view>
         </view>
-      </view>
-    </template>
-    <template v-else>
-      <view
-        class="bg-white rounded-lg p-4 flex justify-between"
-        :style="{
-          marginTop: safeHeight + 20 + 'px',
-        }"
-      >
-        <view>西江船闸调度中心</view>
-        <view class="flex gap-10rpx items-center">
-          <view class="rounded-full bg-green p-3rpx flex items-center">
-            <view class="i-carbon:logo-wechat bg-white"></view>
+      </custom-tab-item>
+
+      <custom-tab-item title="微信联系">
+        <view
+          class="bg-white rounded-lg p-4 flex justify-between"
+          :style="{
+            marginTop: safeHeight + 20 + 'px',
+          }"
+        >
+          <view>西江船闸调度中心</view>
+          <view class="flex gap-10rpx items-center">
+            <view class="rounded-full bg-green p-3rpx flex items-center">
+              <view class="i-carbon:logo-wechat bg-white"></view>
+            </view>
+            <view class="text-green">联系客服</view>
           </view>
-          <view class="text-green">联系客服</view>
         </view>
-      </view>
-    </template>
+      </custom-tab-item>
+    </custom-tab>
   </view>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
+import CustomTab from '@/components/CustomTab.vue'
+import CustomTabItem from '@/components/CustomTabItem.vue'
+
 const { safeAreaInsets } = uni.getSystemInfoSync()
-const activeTag = ref(1)
-const safeHeight = ref(safeAreaInsets?.top + 75)
+const activeTab = ref(0)
+const safeHeight = ref(safeAreaInsets?.top)
 
-// #ifdef MP-WEIXIN
-safeHeight.value = safeHeight.value + 30
-// #endif
-
-function handleSwitchTag(val: number) {
-  activeTag.value = val
-}
 const phoneInfo = ref([
   {
     name: '加售后微信留言',
