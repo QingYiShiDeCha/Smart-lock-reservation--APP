8ebd656ca495ac738af946544c43a055c9460b58
清茶 <2521541557@qq.com>
2025-03-09 00:29:13 +0800

    feat: 全局连接mqtt服务器

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { VueQueryPlugin } from '@tanstack/vue-query'
 import 'virtual:uno.css'
 import { createSSRApp } from 'vue'
 
+import { connectMqtt } from '@/utils/mqtt'
+
 import App from './App.vue'
 import { prototypeInterceptor, requestInterceptor, routeInterceptor } from './interceptors'
 import store from './store'
 
 export function createApp() {
+  connectMqtt('uni_app')
   const app = createSSRApp(App)
   app.use(store)
   app.use(routeInterceptor)
